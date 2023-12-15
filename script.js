f92cb6b3807ebef578c52b2a0534f38d4b6637fa
MillCars <153749791+MillCars@users.noreply.github.com>
2023-12-15 16:34:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 // Открывает модальное окно при загрузке страницы
 window.onload = function () {
     openModal();
-  };
-  
-  // Показывает модальное окно
-  function openModal() {
+};
+
+// Показывает модальное окно
+function openModal() {
     document.getElementById('adModal').style.display = 'block';
-  }
-  
-  // Закрывает модальное окно
-  function closeModal() {
+}
+
+// Закрывает модальное окно
+function closeModal() {
     document.getElementById('adModal').style.display = 'none';
-  }
-  
+}
+
+// Пример объекта с переводами
+var translations = {
+    'en': {
+        'text1-header': 'Best cars for your family and you',
+        'More': 'More',
+        'Contacts': 'Contacts',
+        'Support': 'Support',
+    },
+    'ru': {
+        'text1-header': 'Лучшие машины для твоей семьи и тебя',
+        'More': 'Больше',
+        'Contacts': 'Контакты',
+        'Support': 'Поддержка',
+    }
+};
+
+// Пример изменения текста на странице
+function changeLanguage(lang) {
+    var elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(function(element) {
+        var key = element.getAttribute('data-translate');
+        element.textContent = translations[lang][key];
+    });
+}
